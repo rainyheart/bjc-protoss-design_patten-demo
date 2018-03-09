7be334117d205a2fb0c37f343848b08b8ead8792
zyexjia <yejiazhi27081@billjc.com>
2018-03-09 18:39:04 +0800

    fix compile error

--- test/com/bjc/protoss/dp/wesley/Facade.java
+++ test/com/bjc/protoss/dp/wesley/Facade.java
@@ -3,26 +3,30 @@ package com.bjc.protoss.dp.wesley;
 import org.junit.Test;
 
 import com.bjc.protoss.dp.module.facade.wesley.DataSourceFacade;
+import com.bjc.protoss.dp.module.facade.wesley.UnAuthorizedException;
 import com.bjc.protoss.dp.module.facade.wesley.impl.DataAccessor;
 
 /**
- * 
- * Provide a unified interface to a set of interfaces in a subsystem. Facade
- * defines a higher-level interface that makes the subsystem easier to use.
- * 
- * The pre-condiction is that not changing the existing several intefaces, but build a new one
+ *
+ * Provide a unified interface to a set of interfaces in a subsystem.
+ * Facade
+ * defines a higher-level interface that makes the subsystem easier to
+ * use.
+ *
+ * The pre-condiction is that not changing the existing several
+ * intefaces, but build a new one
  * and consolidate/simplify the interface to caller.
  *
  */
 public class Facade {
-	
-	@Test
-	public void getSetData () {
-		DataSourceFacade dataSource = new DataAccessor();
-		dataSource.setValue("name", "Wesley");
-		dataSource.getValue("name");
-		
-		dataSource.getValue("not exist");
-	}
+
+    @Test
+    public void getSetData() throws UnAuthorizedException {
+        DataSourceFacade dataSource = new DataAccessor();
+        dataSource.setValue("name", "Wesley");
+        dataSource.getValue("name");
+
+        dataSource.getValue("not exist");
+    }
 
 }
